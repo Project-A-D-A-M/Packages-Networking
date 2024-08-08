0382650093e97cf2453a06f5b33f654924c1dc0d
Gabriel-M-Martins <gabrielmmartinscontato@gmail.com>
2024-08-08 16:43:49 -0300

    cityPreFetchDTO add id

--- Sources/Networking/DTOs/CityPreFetchDTO.swift
+++ Sources/Networking/DTOs/CityPreFetchDTO.swift
@@ -7,17 +7,16 @@
 
 import Foundation
 
-
-
-public final class CityPreFetchDTO: Codable {
-    public var id: Int?
-    public var name: String
-    public var state: String
-    public var country: String
-    public var latitude: Double
-    public var longitude: Double
+final class CityPreFetchDTO: Codable {
+    var id: Int
+    var name: String
+    var state: String
+    var country: String
+    var latitude: Double
+    var longitude: Double
     
-    public init(name: String, state: String, country: String, latitude: Double, longitude: Double) {
+    init(id: Int, name: String, state: String, country: String, latitude: Double, longitude: Double) {
+        self.id = id
         self.name = name
         self.state = state
         self.country = country
