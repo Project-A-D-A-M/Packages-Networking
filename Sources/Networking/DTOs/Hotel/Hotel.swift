ede9431c9400d8e8b84fe0e28397e898311ab278
carneijp <carneijp@hotmail.com>
2024-08-09 14:00:56 -0300

    hotel dto

---
+++ Sources/Networking/DTOs/Hotel/Hotel.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joao Paulo Carneiro on 09/08/24.
+//
+
+import Foundation
+public final class Hotel: Codable {
+    public var id: String?
+    public var city: String
+    public var country: String
+    public var month: Int
+    public var year: Int
+    public var hotels: [HotelStar]
+    
+    public init(id: String? = nil, city: String, country: String, hotels: [HotelStar], month: Int, year: Int) {
+        self.id = id
+        self.city = city
+        self.country = country
+        self.hotels = hotels
+        self.month = month
+        self.year = year
+    }
+
+}
+
