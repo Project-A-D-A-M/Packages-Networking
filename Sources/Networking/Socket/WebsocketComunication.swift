449d30a87227773e3b9335a56a844cde3f88a2c5
carneijp <carneijp@hotmail.com>
2024-10-16 16:40:19 -0300

    adicionado estrutura para comunicação de websocket

---
+++ Sources/Networking/Socket/WebsocketComunication.swift
@@ -0,0 +1,24 @@
+//
+//  WebsocketComunication.swift
+//  Networking
+//
+//  Created by Joao Paulo Carneiro on 16/10/24.
+//
+
+public final class WebsocketComunication: Codable {
+    public var communicationID: UUID
+    public var objectID: UUID
+    public var objectType: WebsocketObjectTypes
+    public var operationFlag: WebsocketFlags
+    public var tripID: UUID
+    public var responsibleID: String
+    
+    public init(communicationID: UUID, objectID: UUID, objectType: WebsocketObjectTypes, operationFlag: WebsocketFlags, tripID: UUID, responsibleID: String) {
+        self.communicationID = communicationID
+        self.objectID = objectID
+        self.objectType = objectType
+        self.operationFlag = operationFlag
+        self.tripID = tripID
+        self.responsibleID = responsibleID
+    }
+}
---
+++ Sources/Networking/Socket/WebsocketFlags.swift
@@ -0,0 +1,17 @@
+//
+//  Untitled.swift
+//  Networking
+//
+//  Created by Joao Paulo Carneiro on 16/10/24.
+//
+
+public enum WebsocketFlags: String {
+    case update
+    case isEditing
+    case requestEdit
+    case unavailable
+    case finishedEditing
+    case startbeat
+    case heartbeat
+    case endbeat
+}
