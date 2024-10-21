83095ff8f65d0b1632368ec84750f0608e9e55d2
carneijp <carneijp@hotmail.com>
2024-10-21 15:47:05 -0300

    Add websocket codable error

---
+++ Sources/Networking/Socket/WebSocketCodableError.swift
@@ -0,0 +1,27 @@
+//
+//  WebSocketCodableError.swift
+//  Networking
+//
+//  Created by Joao Paulo Carneiro on 21/10/24.
+//
+
+import Foundation
+
+public enum WebSocketCodableError: Error {
+    case payloadTypeMissmatch
+    case failedToEncode
+    case failedToDecode
+}
+
+public extension WebSocketCodableError: LocalizedError {
+    public var description: String {
+        switch self {
+        case .payloadTypeMissmatch:
+            return NSLocalizedString("Type recived does not match with  ", comment: "typeMissmatch")
+        case .failedToEncode:
+            return NSLocalizedString("Failed to Encode objetc", comment: "failedToEncode")
+        case .failedToDecode:
+            return NSLocalizedString("Failed to Decode objetc", comment: "failedToDecode")
+        }
+    }
+}
