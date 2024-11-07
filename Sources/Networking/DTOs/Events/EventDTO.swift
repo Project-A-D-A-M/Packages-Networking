f48d5a59f4d52bbffb4eb23deab86af83d7d7dcf
carneijp <carneijp@hotmail.com>
2024-11-07 15:50:59 -0300

    work in progress

--- Sources/Networking/DTOs/Events/EventDTO.swift
+++ Sources/Networking/DTOs/Events/EventDTO.swift
@@ -18,18 +18,50 @@ final public class EventDTO: Codable {
     public var type: Types
     public var name: String
     public var address: String
-    public var price: Float
-    public var documents: [UUID]
+    public var documentsIDs: [UUID]
+    public var documents: [DocumentDTO]
+    public var checklist: [ChecklistItemDTO]
+    public var description: String
+    public var links: [LinkDTO]
+    
     public var shareInfoDTO: ShareInfoDTO
     
-    required public init(id: UUID = .init(), date: Date, type: Types, name: String, address: String, price: Float, documents: [UUID], shareInfoDTO: ShareInfoDTO) {
+    required public init(id: UUID, date: Date, type: Types, name: String, address: String, documentsIDs: [UUID], documents: [DocumentDTO], checklist: [ChecklistItemDTO], description: String, links: [LinkDTO], shareInfoDTO: ShareInfoDTO) {
         self.id = id
         self.date = date
         self.type = type
         self.name = name
         self.address = address
-        self.price = price
+        self.documentsIDs = documentsIDs
         self.documents = documents
+        self.checklist = checklist
+        self.description = description
+        self.links = links
         self.shareInfoDTO = shareInfoDTO
+    }
+}
+
+
+final public class LinkDTO: Codable {
+    public var id: UUID
+    public var text: String
+    
+    public init(id: UUID, text: String) {
+        self.id = id
+        self.text = text
+    }
+}
+
+final public class ChecklistItemDTO: Codable {
+    public var id: UUID
+    public var name: String
+    public var checked: Bool
+    public var targetAmount: Int
+    
+    public init(id: UUID, name: String, checked: Bool, targetAmount: Int) {
+        self.id = id
+        self.name = name
+        self.checked = checked
+        self.targetAmount = targetAmount
     }
 }
