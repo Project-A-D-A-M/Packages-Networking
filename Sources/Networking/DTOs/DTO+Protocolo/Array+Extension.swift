74e1051a62dc60c2cd478cc65150450512d86c0c
carneijp <carneijp@hotmail.com>
2024-11-07 16:55:37 -0300

    Modificado o dto do eventos para abraçar a nova solicitação feita pelo gabriel

--- Sources/Networking/DTOs/DTO+Protocolo/Array+Extension.swift
+++ Sources/Networking/DTOs/DTO+Protocolo/Array+Extension.swift
@@ -10,8 +10,4 @@ public extension Array where Element: DTOConvertible {
     func toDTOs() -> [Element.ObjectDTO] {
         return self.compactMap { $0.toDTO() }
     }
-    
-    func fromDTOS() -> [Element] {
-        return self.compactMap { .init($0) }
-    }
 }
